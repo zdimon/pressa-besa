f3999a7e75ca841eb4027538698d10e2d8d01f72
Dmitry Zharikov <zdimon77@gmail.com>
2021-02-24 11:09:50 +0200

    commit from zdimon77@gmail.com ----

---
+++ design/html/index/js/init.js
@@ -0,0 +1,196 @@
+$(document).ready(function() {
+
+    /* detect transit support */
+    let transitFlag = true;
+    if (!Modernizr.cssanimations) {
+        transitFlag = false;
+    }
+
+    /* sliders init */
+    let newestSlider = $('#newestSlider');
+    newestSlider.slick({
+        dots: false,
+        infinite: true,
+        arrows: true,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        prevArrow: $('.newest-slider-prev'),
+        nextArrow: $('.newest-slider-next'),
+        responsive: [
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 620,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 440,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            }
+        ]
+    });
+
+    let actualArticlesSlider = $('#actualArticlesSlider');
+    actualArticlesSlider.slick({
+        dots: false,
+        infinite: true,
+        arrows: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: $('.actual-articles-slider-prev'),
+        nextArrow: $('.actual-articles-slider-next'),
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 620,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 440,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            }
+        ]
+    });
+
+    let booksSlider = $('#booksSlider');
+    booksSlider.slick({
+        dots: false,
+        infinite: true,
+        arrows: true,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        prevArrow: $('.books-slider-prev'),
+        nextArrow: $('.books-slider-next'),
+        responsive: [
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 620,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 440,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            }
+        ]
+    });
+    /* end sliders init */
+
+    /* mobile menu */
+    let burgerMenuLink = $('#burgerMenuLink');
+    let mobMenuOverflow = $('#mobMenuOverflow');
+    let mobMenu = $('#mobMenu');
+    burgerMenuLink.on('click', function (e) {
+
+        e.preventDefault();
+
+        if (!mobMenu.hasClass('mob-menu-opened')) {
+
+            mobMenu.addClass('mob-menu-opened');
+            mobMenuOverflow.stop(true, true).fadeIn(300);
+        }
+    });
+    mobMenuOverflow.on('click', function () {
+
+        if (mobMenu.hasClass('mob-menu-opened')) {
+
+            mobMenu.removeClass('mob-menu-opened');
+            mobMenuOverflow.stop(true, true).fadeOut(300);
+        }
+    });
+    /* end mobile menu */
+
+    /* site header and container sizes */
+    let siteHeader = $('#siteHeader');
+    let siteContainer = $('#siteContainer');
+    $(window).on('load', function () {
+
+        setHeaderAndContainerSizes();
+    });
+    $(window).on('resize', function () {
+
+        setHeaderAndContainerSizes();
+        mobMenu.removeClass('mob-menu-opened');
+        mobMenuOverflow.hide();
+    });
+    function setHeaderAndContainerSizes() {
+
+        let t;
+        clearTimeout(t);
+        t = setTimeout(function () {
+
+            siteContainer.css('padding-top', siteHeader.height() + 'px');
+
+        }, 300);
+    }
+    /* end site header and container sizes */
+
+    /* popup windows */
+    function showForm(formElem) {
+        $(".popup-overlay").fadeIn(fadeSpeed);
+        $(formElem).css({
+            "margin-top": "-" + ($(formElem).height() / 2) + "px",
+            "top": ($(window).height() / 2) + $(window).scrollTop() + "px"
+        }).fadeIn(fadeSpeed);
+    }
+    function hideForm() {
+        $(".popup-overlay").fadeOut(fadeSpeed);
+        $(".popup-wrapper").fadeOut(fadeSpeed);
+    }
+    /* end popup windows */
+});
