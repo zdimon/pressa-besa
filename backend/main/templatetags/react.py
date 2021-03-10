0919d9fea1228d979f54ee19a51841e8ae51e297
Your Name <you@example.com>
2021-03-10 10:15:56 +0300

    commit from zdimon77@gmail.com ----

---
+++ backend/main/templatetags/react.py
@@ -0,0 +1,23 @@
+from django import template
+from django.utils.safestring import mark_safe
+from django.conf import settings
+from bs4 import BeautifulSoup
+register = template.Library() 
+import os
+
+@register.simple_tag
+def react_app():
+    path = os.path.join(settings.BASE_DIR, 'static','build','index.html')
+    with open(path,'r') as f:
+        html = f.read()
+    soup = BeautifulSoup(html, 'html.parser')
+    out = []
+    for js in soup.find_all('script'):
+        script_item = []
+        if js['src'].find('http') > -1: 
+            script_item.append(' src="%s"' % js['src'])
+        else:
+            script_item.append('defer="defer" src="/static/build/%s"' % js['src'])
+        out.append('<script  '+' '.join(script_item)+' ></script>')
+    return mark_safe(''.join(out))
+
