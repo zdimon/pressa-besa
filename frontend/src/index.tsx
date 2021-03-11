56229c24a1081770c9c669f71117731bc50bb007
Dmitry Zharikov <zdimon77@gmail.com>
2021-03-11 11:47:32 +0200

    commit from zdimon77@gmail.com ----

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,19 +3,19 @@ import { render } from "react-dom";
 import UserMenu from './components/account/UserMenu';
 
 // theme
-import { loadTheme } from '@fluentui/react';
+// import { loadTheme } from '@fluentui/react';
 // import { initializeIcons } from '@uifabric/icons';
 
 // initializeIcons();
-loadTheme({
-    semanticColors: {
-        primaryButtonBackgroundHovered: 'silver',
-        buttonBackground: 'silver',
-        inputBackground: '#f7efef'
-    },
-    palette: {
-      themePrimary: '#FE0000'
-    }});
+// loadTheme({
+//     semanticColors: {
+//         primaryButtonBackgroundHovered: 'silver',
+//         buttonBackground: 'silver',
+//         inputBackground: '#f7efef'
+//     },
+//     palette: {
+//       themePrimary: '#FE0000'
+//     }});
 //////////////////
 
 const loginEl = document.getElementById("userMenu");
