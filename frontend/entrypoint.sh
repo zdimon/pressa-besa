49f2ef6564b3986dfd2e9c726e9345e8251054bf
Dmitry Zharikov <zdimon77@gmail.com>
2021-03-11 10:04:23 +0200

    commit from zdimon77@gmail.com ----

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Start frontend container"
-echo "Change user permissions $(id -u):$(id -g)"
-chown $(id -u):$(id -g) -R /app/node_modules
+#echo "Change user permissions $(id -u):$(id -g)"
+#chown $(id -u):$(id -g) -R /app/node_modules
 npm install
 npm run build
