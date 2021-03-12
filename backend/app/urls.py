079f74a1483832fb7fc3226c16b3a9b0c51db9a6
Your Name <you@example.com>
2021-03-12 16:30:48 +0300

    commit from zdimon77@gmail.com ----

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -31,7 +31,6 @@ urlpatterns = [
     path('', index),
     path('v1/', include([
         path('account/', include('account.urls'))
-        path('journal/', include('journal.urls'))
     ])),
     path('api', schema_view.with_ui('swagger', cache_timeout=0), 
     name='schema-swagger-ui'),
