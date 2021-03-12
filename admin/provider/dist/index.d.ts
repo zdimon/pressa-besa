8766c64d8a20a3bd40550bbe501ceea3b25b02a6
Your Name <you@example.com>
2021-03-12 15:44:43 +0300

    commit from zdimon77@gmail.com ----

--- admin/provider/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Sort, DataProvider } from 'ra-core';
-export { default as tokenAuthProvider, fetchJsonWithAuthToken, } from './tokenAuthProvider';
-export declare const getOrderingQuery: (sort: Sort) => {
-    ordering: string;
-};
-declare const _default: (apiUrl: String, httpClient?: Function) => DataProvider;
-export default _default;
