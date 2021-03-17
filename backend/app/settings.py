60841599ced910d6e25ce96070881171744ec8a5
Dmitry Zharikov <zdimon77@gmail.com>
2021-03-17 10:30:29 +0200

    commit from zdimon77@gmail.com ----

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -71,12 +71,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
@@ -164,3 +159,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'app.pagination.PageNumberWithPageSizePagination',
     'PAGE_SIZE': 10
 }
+
+from .local import *
