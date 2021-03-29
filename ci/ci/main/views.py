bea12987bf2ca264a9680daa55eceea9d46e8eda
Your Name <you@example.com>
2021-03-29 15:38:53 +0300

    commit from zdimon77@gmail.com ----

--- ci/ci/main/views.py
+++ ci/ci/main/views.py
@@ -1,21 +1,26 @@
 from django.shortcuts import render
 from .forms import EnvForm
 from django.shortcuts import redirect
+from django.conf import settings
+from .models import Env
+from .tasks import normalize_email
 
 
 def index(request):
     if request.method == 'POST':
         form = EnvForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('/done')
+            obj = form.save()
+            return redirect('/done/%s' % obj.id)
     else:
         form = EnvForm()
     return render(request, 'index.html', {"form": form})
 
 
-def done(request):
-    return render(request, 'done.html')
+def done(request, id):
+    env = Env.objects.get(pk=id)
+    link = '%s%s' % (normalize_email(env.email), settings.DOMAIN)
+    return render(request, 'done.html', {'link': link})
 
 
 def info(request):
