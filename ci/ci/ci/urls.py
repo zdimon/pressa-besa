ea79c0d8071f100fc9ea65271c866b3ce22bb473
Your Name <you@example.com>
2021-04-03 14:06:45 +0300

    commit from zdimon77@gmail.com ----

--- ci/ci/ci/urls.py
+++ ci/ci/ci/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from main.views import index, done, logout_view, env, tasks, take_task, del_task, done_task, end_task, instr, hook, merge_master
+from main.views import index, done, logout_view, env, tasks, take_task, del_task, done_task, end_task, instr, hook, merge_master, maket
 
 urlpatterns = [
     path('', index),
@@ -29,6 +29,7 @@ urlpatterns = [
     path('tasks', tasks),
     path('instr', instr),
     path('hook', hook),
+    path('maket', maket),
     path('task/take/<int:id>', take_task),
     path('task/del/<int:id>', del_task),
     path('task/done/<int:id>', done_task),
