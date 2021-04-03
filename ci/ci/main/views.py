da86cd6df35ae9669d6dd275eb343ef67bf794d3
Your Name <you@example.com>
2021-04-03 14:07:23 +0300

    commit from zdimon77@gmail.com ----

--- ci/ci/main/views.py
+++ ci/ci/main/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import redirect
 from django.conf import settings
 from .models import Env
 from .tasks import normalize_email, git_push, git_merge_with_master
-from .models import Env, Task, Task2User, Commit
+from .models import Env, Task, Task2User, Commit, Maket
 from git import Repo
 from django.shortcuts import redirect
 from django.contrib.auth import logout
