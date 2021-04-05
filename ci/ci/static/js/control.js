535f38bd0d6857f68a7043965bba0d17ea1aca12
Dmitry Zharikov <zdimon77@gmail.com>
2021-04-05 17:09:43 +0300

    auto

--- ci/ci/static/js/control.js
+++ ci/ci/static/js/control.js
@@ -2,6 +2,9 @@ $( document ).ready(function() {
    
     $('.git-pull').on('click', (e) => {
         id = $(e.target).attr('data-id');
+        
+        $('#git-pull-result').html('выполняется....');
+        $('#git-pull-error').html('');
         const url = `/git/pull/${id}`;
         $.ajax({
             url: url,
