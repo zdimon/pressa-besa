5e5b5bf26ed23963dee811f37c97870884c02ba3
Dmitry Zharikov <zdimon77@gmail.com>
2021-04-06 11:58:51 +0300

    auto

--- ci/ci/static/js/control.js
+++ ci/ci/static/js/control.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
    
     $('.git-pull').on('click', (e) => {
         id = $(e.target).attr('data-id');
-        
+        $('#git-oper').html('операция: git pull');
         $('#git-pull-result').html('выполняется....');
         $('#git-pull-error').html('');
         const url = `/git/pull/${id}`;
@@ -20,7 +20,7 @@ $( document ).ready(function() {
 
     $('.git-status').on('click', (e) => {
         id = $(e.target).attr('data-id');
-        
+        $('#git-oper').html('операция: git status');
         $('#git-pull-result').html('выполняется....');
         $('#git-pull-error').html('');
         const url = `/git/status/${id}`;
@@ -37,7 +37,7 @@ $( document ).ready(function() {
     
     $('.git-push').on('click', (e) => {
         id = $(e.target).attr('data-id');
-        
+        $('#git-oper').html('операция: git push');
         $('#git-pull-result').html('выполняется....');
         $('#git-pull-error').html('');
         const url = `/git/push/${id}`;
@@ -54,7 +54,7 @@ $( document ).ready(function() {
 
     $('.git-diff').on('click', (e) => {
         id = $(e.target).attr('data-id');
-        
+        $('#git-oper').html('операция: git diff');
         $('#git-pull-result').html('выполняется....');
         $('#git-pull-error').html('');
         const url = `/git/diff/${id}`;
