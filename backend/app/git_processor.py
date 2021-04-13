7da88de9c3c9ecf0dcec67bca3cc8f6d4960d96b
Dmitry Zharikov <zdimon77@gmail.com>
2021-04-13 11:04:23 +0300

    commit from zdimon77@gmail.com ----

--- backend/app/git_processor.py
+++ backend/app/git_processor.py
@@ -3,6 +3,6 @@ from django.conf import settings
 import os
 
 def current_branch(request):
-    local_repo = Repo(path=os.path.join(settings.BASE_DIR, '..')
+    local_repo = Repo(path=os.path.join(settings.BASE_DIR, '..'))
     local_branch = local_repo.active_branch.name
     return {'git_branch': local_branch}
