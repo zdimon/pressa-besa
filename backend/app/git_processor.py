30d5fcfced433756dd5aa07a07a0562ebd9905f8
Dmitry Zharikov <zdimon77@gmail.com>
2021-04-13 09:36:06 +0300

    commit from zdimon77@gmail.com ----

---
+++ backend/app/git_processor.py
@@ -0,0 +1,8 @@
+from git import Repo
+from django.conf import settings
+import os
+
+def current_branch(request):
+    local_repo = Repo(path=os.path.join(settings.GIT_PATH))
+    local_branch = local_repo.active_branch.name
+    return {'git_branch': local_branch}
--- backend/journal/views.py
+++ backend/journal/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
+from journal.models import Journal, Issue
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
 
-# Create your views here.
+
+class JournalView(DetailView):
+    model = Journal
+    slug_url_kwarg = 'name_slug'
+    slug_field = 'name_slug'
+
+    def get_customer(self):
+        try:
+            if not self.request.user.is_anonymous():
+                return self.request.user.customer
+            else:
+                return None
+        except ObjectDoesNotExist:
+            return None
+
+    def get_context_data(self, **kwargs):
+        self.customer = self.get_customer()
+        issue_name_slug = self.kwargs.get('issue_name_slug', None)
+        try:
+            if issue_name_slug:
+                self.current_issue = self.object.active_issue_set.get(
+                    name_slug=issue_name_slug)
+            else:
+                self.current_issue = self.object.last_issue
+        except:
+            raise Http404
+
+        context = super(JournalView, self).get_context_data(**kwargs)
+        return context
