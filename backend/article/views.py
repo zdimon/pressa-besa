c68db11b6533bd82ead2c089a8a9cddbc69966d6
Dmitry Zharikov <zdimon77@gmail.com>
2021-04-29 06:56:34 +0300

    auto

--- backend/article/views.py
+++ backend/article/views.py
@@ -4,7 +4,7 @@ from .utils import make_cover
 
 
 def article_list(request):
-    arts = Article.objects.all().order_by('-id')[0:10]
+    arts = Article.objects.all().order_by('-id')[0:20]
     return render(request, 'article/article_list.html', {'arts': arts})
 
 
