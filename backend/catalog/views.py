f396aa4c75486c15964620a0e8d7d5c99a1344d6
Your Name <you@example.com>
2021-04-29 10:23:01 +0300

    auto

--- backend/catalog/views.py
+++ backend/catalog/views.py
@@ -3,6 +3,6 @@ from journal.models import Journal
 from .models import Category
 
 def category_detail(request, journal_type, category):
-    journals = Journal.objects.all()[0:10]
+    journals = Journal.objects.all().order_by('-id')[0:15]
     categories = Category.objects.all()
     return render(request, 'catalog/category_detail.html', {"journals": journals, "categories": categories})
