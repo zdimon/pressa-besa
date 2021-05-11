8364c462fdbd380d0a43833689d152527efd6fdc
Dmitry Zharikov <zdimon77@gmail.com>
2021-05-11 10:44:51 +0300

    auto

--- backend/catalog/views.py
+++ backend/catalog/views.py
@@ -4,7 +4,7 @@ from .models import Category
 
 def category_detail(request, journal_type, category):
     #new_issues = Issue.objects.filter(is_public=True).order_by('-id')[0:15]
-    journals = Journal.objects.all().order_by('-last_issue_id')[0:15]
+    journals = Journal.objects.filter(journal_type=journal_type).order_by('-last_issue_id')[0:15]
     popular = Journal.objects.filter(is_popular=True).order_by('-id')[0:15]
     categories = Category.objects.all()
     return render(request, 'catalog/category_detail.html', {"journals": journals, "categories": categories, "popular": popular})
