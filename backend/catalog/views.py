aa0574eec66b077d426ec7d8ad7d4a54cfad09c6
Dmitry Zharikov <zdimon77@gmail.com>
2021-05-11 08:51:17 +0300

    auto

--- backend/catalog/views.py
+++ backend/catalog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from journal.models import Journal
+from journal.models import Journal, Issue
 from .models import Category
 
 def category_detail(request, journal_type, category):
+    new_issues = Issue.objects.filter(is_public=True).order_by('-id')[0:15]
     journals = Journal.objects.all().order_by('-id')[0:15]
     popular = Journal.objects.filter(is_popular=True).order_by('-id')[0:15]
     categories = Category.objects.all()
-    return render(request, 'catalog/category_detail.html', {"journals": journals, "categories": categories, "popular": popular})
+    return render(request, 'catalog/category_detail.html', {"journals": journals, "categories": categories, "popular": popular, "new_issues": new_issues})
