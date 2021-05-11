ddb23fc6d1a16391bf9d70fa832c587640ae45a6
Dmitry Zharikov <zdimon77@gmail.com>
2021-05-11 09:55:35 +0300

    auto

--- backend/catalog/views.py
+++ backend/catalog/views.py
@@ -7,4 +7,4 @@ def category_detail(request, journal_type, category):
     journals = Journal.objects.all().order_by('-last_issue_id')[0:15]
     popular = Journal.objects.filter(is_popular=True).order_by('-id')[0:15]
     categories = Category.objects.all()
-    return render(request, 'catalog/category_detail.html', {"journals": journals, "categories": categories, "popular": popular, "new_issues": new_issues})
+    return render(request, 'catalog/category_detail.html', {"journals": journals, "categories": categories, "popular": popular})
