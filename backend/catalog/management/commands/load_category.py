0f2e8821576b596f34802c32c16d07a788c75e7c
Dmitry Zharikov <zdimon77@gmail.com>
2021-05-11 16:23:05 +0300

    auto

---
+++ backend/catalog/management/commands/load_category.py
@@ -0,0 +1,55 @@
+from django.core.management.base import BaseCommand
+from catalog.models import Category
+from django.conf import settings
+import os
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        print('Loading category')
+        Category.objects.all().delete()
+        Journal.objects.all().delete()
+
+        names = ['ЖЕНСКИЕ', 'МУЖСКИЕ', 'ДЕТСКИЕ', 'ДОМ И СЕМЬЯ']
+        cntc = 0
+        for name in names:
+            cntc += 1
+            c = Category()
+            c.name = name
+            c.save()
+            print('Saving %s' % name)
+
+            journal_list = ['Наука и техника', 'Здоровье', 'Моделист-конструктор', 'Лиза', 'Прибой', 'Забой', 'Внуки и правнуки', 'В мире муравья']
+            cnt = 0
+            for jname in journal_list:
+                cnt += 1
+                j = Journal()
+                j.name = jname+" "+str(cnt)+str(cntc)
+                j.is_public = True
+                j.show_in_books = True
+                j.is_popular = True
+                j.save()
+                j.category.add(c)
+                path = os.path.join(settings.BASE_DIR,'init_data','journal',f'{cnt}.jpg')
+                with open(path, 'rb') as doc_file:
+                    j.default_cover.save(f'{cnt}.jpeg', File(doc_file), save=True)
+
+                for ni in range(1,20):
+                    name = f'выпуск {ni}'
+                    i = Issue()
+                    i.name = name
+                    i.is_public = True
+                    i.journal = j
+                    i.save()
+
+                    for pi in range(1,10):
+                        p = IssuePage()
+                        p.paper = i
+                        p.page = pi
+                        p.save()
+                        path = os.path.join(settings.BASE_DIR,'init_data','journal',f'{pi}.jpg')
+                        with open(path, 'rb') as doc_file:
+                            p.file_low.save(f'{pi}.jpeg', File(doc_file), save=True)
+                            p.file_middle.save(f'{pi}.jpeg', File(doc_file), save=True)
+                            p.file_high.save(f'{pi}.jpeg', File(doc_file), save=True)
+        
--- backend/catalog/views.py
+++ backend/catalog/views.py
@@ -5,8 +5,15 @@ from .utils import get_journal_type
 
 def category_detail(request, journal_type, category):
     #new_issues = Issue.objects.filter(is_public=True).order_by('-id')[0:15]
-    print(journal_type)
-    journals = Journal.objects.filter(journal_type=get_journal_type(journal_type)).order_by('-last_issue_id')[0:15]
+    journals_query = Journal.objects.filter(journal_type=get_journal_type(journal_type))
+
+    if category != 'categories':
+        category = Category.objects.get(name_slug=category)
+        journals_query.filter(category=category)
+
+    
+    journals = journals_query.order_by('-last_issue_id')[0:15]
+    
     popular = Journal.objects.filter(is_popular=True).order_by('-id')[0:15]
     categories = Category.objects.filter(show_in_new_catalog=True)
     return render(request, 'catalog/category_detail.html', {"journals": journals, "categories": categories, "popular": popular, 'journal_type': journal_type})
