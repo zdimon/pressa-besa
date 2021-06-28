624d2b2d3ebb7afcbb999102f21c868b4b6402ff
Your Name <you@example.com>
2021-06-28 12:54:12 +0300

    auto

--- backend/catalog/views.py
+++ backend/catalog/views.py
@@ -3,11 +3,12 @@ from journal.models import Journal, Issue
 from .models import Category
 from .utils import get_journal_type
 from django.core.paginator import Paginator
+from announce.models import News
 
 def category_detail(request, journal_type, category):
     #new_issues = Issue.objects.filter(is_public=True).order_by('-id')[0:15]
     page = request.GET.get('page',1)
-    
+    news = News.objects.all().order_by('-id')[0:10]
 
     jtype = get_journal_type(journal_type)
     if jtype != 'all':
@@ -34,4 +35,4 @@ def category_detail(request, journal_type, category):
     
     popular = Journal.objects.filter(is_popular=True).order_by('-id')[0:15]
     categories = Category.objects.filter(show_in_new_catalog=True)
-    return render(request, 'catalog/category_detail.html', {"journals": journals, "categories": categories, "popular": popular, 'journal_type': journal_type, "is_show_popular": is_show_popular, "category": category, "page_obj": page_obj, "jtype": jtype})
+    return render(request, 'catalog/category_detail.html', {"journals": journals, "categories": categories, "popular": popular, 'journal_type': journal_type, "is_show_popular": is_show_popular, "category": category, "page_obj": page_obj, "jtype": jtype, 'news': news})
