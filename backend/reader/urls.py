4913a043d70d1889dd6fd1a863c60ec7fedb451e
Your Name <you@example.com>
2021-08-20 14:47:44 +0300

    auto

--- backend/reader/urls.py
+++ backend/reader/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import reader_index
 
 urlpatterns = [
-    path('reader', reader_index, name="reader")
+    path('reader/<int:issue_id>', reader_index, name="reader")
 ]
--- backend/reader/views.py
+++ backend/reader/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-
+from django.core.exceptions import ObjectDoesNotExist
+from journal.models import Issue, IssuePage
+from django.http import Http404
 # Create your views here.
 
-def reader_index(request):
-    return render(request,'reader/index.html')
+
+def reader_index(request, issue_id):
+    try:
+        issue = Issue.objects.get(pk=issue_id)
+    except ObjectDoesNotExist:
+        raise Http404
+    pages = IssuePage.objects.filter(paper=issue)
+    cnt = {"issue": issue, "pages": pages}
+    return render(request,'reader/index.html', cnt)
