3bbafa4a8e74375df2f7e7eb84f7078cadb4836c
Your Name <you@example.com>
2021-09-02 15:41:10 +0300

    auto

--- frontend/src/components/reader/TextReader.tsx
+++ frontend/src/components/reader/TextReader.tsx
@@ -1,15 +1,51 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Request } from '../../Request';
 
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
 
 
 export default function TextReader(props) {
+
+  const [open_pay_dialog, setOpenPayDialog] = React.useState(false);
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
 
   const [articles, setArticles] = React.useState([]);
   const [current_article, setCurrentArticle] = React.useState({});
   const [show_current, setShowCurrent] = React.useState(false);
   const [show_image, setShowImage] = React.useState(false);
   const req = new Request();
+
+  const handleClose = () => {
+    setOpenPayDialog(false);
+  };
 
   const selectArticle = (article_id) => {
 
@@ -40,7 +76,7 @@ export default function TextReader(props) {
   }, []);
 
     return (
-    
+      <>
       <section className="section section-xl bg-gradient-gray">
         <div className="container position-relative">
 
@@ -79,7 +115,10 @@ export default function TextReader(props) {
                 <div className="article-thumbnail">
                     <div className="article-thumbnail__body">
                         <div className="article-thumbnail__body-caption">
-                            <a href="#" onClick={() => selectArticle(item.id)} >{item.title}</a>
+                            {props.isPaid?
+                               <a href="#" onClick={() => selectArticle(item.id)} >{item.title}</a>:
+                               <a href="#" onClick={() => setOpenPayDialog(true)} >{item.title}</a>
+                            }
                             <p>
                                 {item.short_text}
                             </p>
@@ -93,5 +132,19 @@ export default function TextReader(props) {
           </div>
         </div>
       </section>
+
+      <Modal
+      open={open_pay_dialog}
+      onClose={handleClose}
+      >
+      <div  className={classes.paper} style={modalStyle}>
+      <h2 id="simple-modal-title">Вы не оплатили выпуск</h2>
+      <p id="simple-modal-description">
+      <a href="/lk">Перейдите для оплаты</a>
+      </p>
+
+      </div>
+      </Modal>
+      </>
     )
 }
