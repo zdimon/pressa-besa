87e7b42f191c7cd7758dcc964a9aead47b952058
Your Name <you@example.com>
2021-10-06 18:07:14 +0300

    auto

--- frontend/src/components/i18n/LangSwitcher.tsx
+++ frontend/src/components/i18n/LangSwitcher.tsx
@@ -6,7 +6,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   formControl: {
     marginLeft: 20,
-    marginBottom: 20,
+    marginBottom: 11,
     minWidth: 40,
     marginTop: 0,
   },
