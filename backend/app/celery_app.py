beed2b93d5f9a94d772c2cc8e3ad1bd1484dfffe
Your Name <you@example.com>
2021-11-08 20:51:03 +0300

    auto

--- backend/app/celery_app.py
+++ backend/app/celery_app.py
@@ -1,6 +1,8 @@
 from celery import Celery
 from django.conf import settings
 import os
+from celery.schedules import crontab
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
@@ -8,4 +10,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 app = Celery()
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+app.conf.beat_schedule = {
+    # Executes every Monday morning at 7:30 a.m.
+    'convert-articles-audio': {
+        'task': 'article.tasks.convert_atricles_to_ogg',
+        'schedule': crontab(minute='*/1')
+    },
+}
+
 
