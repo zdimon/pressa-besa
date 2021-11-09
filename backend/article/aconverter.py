ae53f35f0faf01e772add96c4af3cd281a67c393
Your Name <you@example.com>
2021-11-09 11:27:37 +0300

    commit from zdimon77@gmail.com ----

--- backend/article/aconverter.py
+++ backend/article/aconverter.py
@@ -53,7 +53,9 @@ def synthesize(iam_token, text):
 
     with requests.post(url, headers=headers, data=data, stream=True) as resp:
         if resp.status_code != 200:
-            raise RuntimeError("Invalid response received: code: %d, message: %s" % (resp.status_code, resp.text))
+            print('Error converting')
+            return False
+            #raise RuntimeError("Invalid response received: code: %d, message: %s" % (resp.status_code, resp.text))
 
         for chunk in resp.iter_content(chunk_size=None):
             yield chunk
