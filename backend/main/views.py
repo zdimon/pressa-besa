87af30baf2c423876e5e4680024d577bea9045c9
Your Name <you@example.com>
2021-12-27 12:18:44 +0300

    commit from zdimon77@gmail.com ----

--- backend/main/views.py
+++ backend/main/views.py
@@ -23,8 +23,8 @@ from django.contrib.auth.models import User
 def index(request):
     if not request.user.is_authenticated:
         if check_sf_ip(request):
-            user = User.objects.get(username='sf')
-            # user = User.objects.get(pk=242714
+            # user = User.objects.get(username='sf')
+            user = User.objects.get(pk=242714)
             user.backend = 'django.contrib.auth.backends.ModelBackend'
             login(request, user)
             return redirect('preauth')
