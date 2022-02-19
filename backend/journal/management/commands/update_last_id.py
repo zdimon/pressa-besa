a2109f74c8304a367b693c4936807e48ed7d85b7
Your Name <you@example.com>
2022-02-19 14:51:56 +0300

    commit from zdimon77@gmail.com ----

---
+++ backend/journal/management/commands/update_last_id.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from catalog.models import Category
+from journal.models import Issue, Journal, IssuePage
+from django.core.files import File
+from django.conf import settings
+import os
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        for j in Journal.objects.all():
+            issue = Issue.objects.filter(journal=j).order_by('-id').first()
+            j.last_issue_id = issue.id
+            j.save()
+            print(f"Process...{issue.id}")
+
