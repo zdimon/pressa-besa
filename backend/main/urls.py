645a7eb61c94d6db5bea5896a993023e56961e0a
Your Name <you@example.com>
2022-02-20 14:58:39 +0300

    publisher

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import search, JournalBuyView, signin, register, signin, sf
+from .views import search, JournalBuyView, signin, register, signin, sf, publisher
 
 
 urlpatterns = [
     path('signin', signin, name="signin"),
+    path('publisher/<slug:name_slug>', publisher, name="publisher"),
     path('<slug:name_slug>/<slug:issue_name_slug>/buy', JournalBuyView.as_view(), name="journal-buy"),
     path('search', search, name="search"),
     path('register', register, name="register"),
