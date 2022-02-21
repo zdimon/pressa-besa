74966cc14bf1a7df62ae81390ef30ce3bb0ed166
Your Name <you@example.com>
2022-02-21 14:55:16 +0300

    pdf bookmark

---
+++ frontend/src/components/bookmarks/pdfBookmark.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+import { Request } from '../../Request';
+import Snackbar from '@material-ui/core/Snackbar';
+
+
+export default function PdfBookmark(props) {
+    
+    const [message, setMessage] = React.useState('');
+
+    const [state, setState] = React.useState({
+        open: false,
+        vertical: 'top',
+        horizontal: 'center',
+      });
+    const { vertical, horizontal, open } = state;
+
+    const handleClose = () => {
+        setState({ ...state, open: false });
+      };
+
+    const submit = function() {
+        // console.log(props.issueId);
+        if(localStorage.getItem("token")) {
+            const req = new Request();
+
+            const data = {
+                "issue_id": props.issueId,
+                "type": "issue"
+            }
+
+            setState({ ...state, open: true});
+            req.post('bookmarks/add',data)
+            .then((payload) => {
+                
+                console.log(payload);
+                setMessage(payload.message);
+            }
+            );
+        } else {
+            var el = document.getElementById('js-login-header-link');
+            el.dispatchEvent(
+              new MouseEvent('click', {
+                  view: window,
+                  bubbles: true,
+                  cancelable: true,
+                  buttons: 1
+              })
+            )
+        }
+    }
+    return (
+        <>
+       
+        <label onClick={submit} className="custom-checkbox-label padding-little-top"></label>
+
+        <Snackbar
+            open={open}
+            onClose={handleClose}
+            message={message}
+            autoHideDuration={2000}
+        />
+        </>
+    )
+}
